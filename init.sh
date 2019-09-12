733de940e4a097ffdb1513733e2600b81df709a3
Kenny Pitt <kenny.pitt@gmail.com>
2019-09-12 07:03:40 -0400

    init: Update base repo directory name

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
-ln -s ~/dotfiles/vim/vimrc ~/.vimrc
-ln -s ~/dotfiles/vim/gvimrc ~/.gvimrc
-ln -s ~/dotfiles/vim/vimfiles ~/.vim
+ln -s ~/.dotfiles/vim/vimrc ~/.vimrc
+ln -s ~/.dotfiles/vim/gvimrc ~/.gvimrc
+ln -s ~/.dotfiles/vim/vimfiles ~/.vim
