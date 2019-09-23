228b7fc8807f66f51e30cc7692b0fa93bbcbcebd
Kenny Pitt <kenny.pitt@gmail.com>
2019-09-22 21:58:46 -0400

    init: Generalize init script to use template files

--- init.sh
+++ init.sh
@@ -1,3 +1,41 @@
-ln -s ~/.dotfiles/vim/vimrc ~/.vimrc
-ln -s ~/.dotfiles/vim/gvimrc ~/.gvimrc
-ln -s ~/.dotfiles/vim/vimfiles ~/.vim
+#!/usr/bin/env bash
+cd $(dirname $0)
+DOTFILES_ROOT=$(pwd)
+echo $DOTFILES_ROOT
+
+set -e
+
+copy_file () {
+    local src=$1 dst=$2
+
+    if [ -f "$dst" -o -d "$dst" -o -L "$dst" ]
+    then
+        printf "File already exists: $dst -> $src\n"
+    else
+        cp -p "$src" "$dst"
+        printf "copied $src to $dst\n"
+    fi
+}
+
+link_file () {
+    local src=$1 dst=$2
+
+    if [ -f "$dst" -o -d "$dst" -o -L "$dst" ]
+    then
+        printf "File already exists: $dst -> $src\n"
+    else
+        src=${src#$HOME/}
+        ln -s "$src" "$dst"
+        printf "linked $src to $dst\n"
+    fi
+}
+
+# Copy all config file templates to the correct locations.
+for src in $(find -H "$DOTFILES_ROOT" -maxdepth 2 -name '*-template')
+do
+    dst="$HOME/.$(basename "${src%-*}")"
+    copy_file "$src" "$dst"
+done
+
+# Symlink config directories.
+link_file "$DOTFILES_ROOT/vim/vimfiles" "$HOME/.vim"
