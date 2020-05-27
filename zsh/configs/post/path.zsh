92328f6af4d02bd9b1b1f333a28001148b85d533
Kenny Pitt <kenny.pitt@microfocus.com>
2020-05-27 11:32:37 -0400

    zsh: Additional user bin directory options

--- zsh/configs/post/path.zsh
+++ zsh/configs/post/path.zsh
@@ -1,3 +1,9 @@
+# Prepend user-specific PATH directories, if they exist.
+# Each directory is added to the beginning of the path, so
+# these should be in reverse order of priority.
+[[ -d $HOME/.local/bin ]] && PATH="$HOME/.local/bin:$PATH"
+[[ -d $HOME/bin ]] && PATH="$HOME/bin:$PATH"
+
 # ensure dotfiles bin directory is loaded first
 PATH="$HOME/.bin:/usr/local/sbin:$PATH"
 
