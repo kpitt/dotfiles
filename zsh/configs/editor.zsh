823fbda278d446d30e2bb0fe8fda8176ee7f8d4f
Kenny Pitt <kenny.pitt@microfocus.com>
2020-05-29 16:05:56 -0400

    zsh: Configure `vim` as the default editor

---
+++ zsh/configs/editor.zsh
@@ -0,0 +1,6 @@
+# Set the default editor.
+# Don't set this to `gvim` or `mvim`, it won't work.
+# Don't set this to `gvim -f`, it won't work (unless you wrap it in a script).
+# http://robots.thoughtbot.com/visual-ize-the-future
+export VISUAL=vim
+export EDITOR=$VISUAL
