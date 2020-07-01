b7173cb40170ef82d0537be07bfd92c65a8aa926
Kenny Pitt <kenny.pitt@gmail.com>
2020-07-01 10:58:33 -0400

    zsh: Add `.zshenv` file to set Homebrew path
    Non-interactive subshells only load `.zshenv`, not `.zshrc`.  This file
    ensures that subshells in Vim and other tools have Homebrew-installed
    commands in the path.

--- zsh/configs/post/path.zsh
+++ zsh/configs/post/path.zsh
@@ -4,8 +4,11 @@
 [[ -d $HOME/.local/bin ]] && PATH="$HOME/.local/bin:$PATH"
 [[ -d $HOME/bin ]] && PATH="$HOME/bin:$PATH"
 
+# prepend Hombebrew directories to path
+PATH="/usr/local/bin:/usr/local/sbin:$PATH"
+
 # ensure dotfiles bin directory is loaded first
-PATH="$HOME/.bin:/usr/local/sbin:$PATH"
+PATH="$HOME/.bin:$PATH"
 
 # mkdir .git/safe in the root of repositories you trust
 PATH=".git/safe/../../bin:$PATH"
---
+++ zshenv
@@ -0,0 +1,4 @@
+# Vim only sources zshenv (this file), not zshrc.
+
+# Add the Homebrew path here so that Vim can find Homebrew's ctags.
+PATH=/usr/local/bin:/usr/local/sbin:$PATH
