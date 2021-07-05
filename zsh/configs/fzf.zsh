c2b7405a733927541b91ee1e5a7370a255ae6c35
Kenny Pitt <kenny.pitt@gmail.com>
2021-07-04 20:20:50 -0400

    fzf: Follow symlink's by default

--- zsh/configs/fzf.zsh
+++ zsh/configs/fzf.zsh
@@ -12,7 +12,7 @@ fi
 
 # Note that these FZF options are used by fzf.vim automatically! Yay!
 # Use a separate tool to smartly ignore files
-export FZF_DEFAULT_COMMAND='rg --hidden --files'
+export FZF_DEFAULT_COMMAND='rg --hidden --follow --files'
 # Jellybeans theme: https://github.com/junegunn/fzf/wiki/Color-schemes
 export FZF_DEFAULT_OPTS='--color fg:188,bg:233,hl:103,fg+:222,bg+:234,hl+:104
 --color info:183,prompt:110,spinner:107,pointer:167,marker:215
