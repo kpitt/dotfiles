0ac7be71f012d9d43130a576af30e5a5adb23c24
Kenny Pitt <kenny.pitt@gmail.com>
2022-01-19 08:13:09 -0500

    brew: Additional config environment variables
    Reduce auto-update frequency and hide extra configuration hints on newer
    versions of Homebrew.  Also use the `bat` utility to improve the output
    if using the `brew cat` command.

--- zsh/configs/homebrew.zsh
+++ zsh/configs/homebrew.zsh
@@ -1,4 +1,11 @@
-# To opt in to Homebrew analytics, `unset` this in ~/.zshrc.local .
-# Learn more about what you are opting in to at
-# https://docs.brew.sh/Analytics
+# Opt out of Homebrew analytics.
 export HOMEBREW_NO_ANALYTICS=1
+
+# Only run autoupdate once a day.
+export HOMEBREW_AUTOUPDATE_SECS=86400
+
+# Use `bat` for the `brew cat` command.
+export HOMEBREW_BAT=1
+
+# Disable additional hints about environment config.
+export HOMEBREW_NO_ENV_HINTS=1
