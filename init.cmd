60e872e2ee0eb66174b06cae6c9883481152c575
Kenny Pitt <kenny.pitt@gmail.com>
2022-06-18 14:32:42 -0400

    chez: Remove Windows `init.cmd` script
    This script is basically worthless at this point.  It does very little
    to start with, and it isn't compatible with the chezmoi file naming
    scheme.  Besides, chezmoi supports Windows so we should be able to come
    up with something much better, if needed.

--- init.cmd
+++
@@ -1,5 +0,0 @@
-mklink %HOME%\_vimrc %HOME%\.dotfiles\vimrc
-mklink %HOME%\_gvimrc %HOME%\.dotfiles\gvimrc
-mklink /D %HOME%\vimfiles %HOME%\.dotfiles\vim
-
-mklink %HOME%\.gitconfig %HOME%\.dotfiles\gitconfig
