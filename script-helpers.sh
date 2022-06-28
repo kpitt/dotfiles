559d178f66249c6bfffd0905bf334917cfeee3e3
Kenny Pitt <kenny.pitt@gmail.com>
2022-06-28 13:43:55 -0400

    chez: Enhance/clean up install scripts

--- script-helpers.sh
+++ script-helpers.sh
@@ -16,7 +16,9 @@ else
 fi
 tty_mkbold() { tty_escape "1;$1"; }
 tty_underline="$(tty_escape "4;39")"
-tty_blue="$(tty_mkbold 34)"
+tty_green="$(tty_escape 32)"
+tty_blue="$(tty_escape 34)"
+tty_default="$(tty_escape 39)"
 tty_bold="$(tty_mkbold 39)"
 tty_reset="$(tty_escape 0)"
 
@@ -35,23 +37,52 @@ info() {
   printf "${tty_blue}==>${tty_bold} %s${tty_reset}\n" "$(shell_join "$@")"
 }
 
+title() {
+  printf "${tty_green}==>${tty_bold} %s${tty_reset}\n" "$(shell_join "$@")"
+}
+
+identifier() {
+  printf "${tty_green}%s${tty_default}" "$1"
+}
+
+installing_feature() {
+  title "Installing $(identifier $1)"
+}
+
 execute() {
   if ! "$@"; then
     abort "$(printf "Failed during: %s" "$(shell_join "$@")")"
   fi
 }
 
-stay_awake_while(){
+stay_awake_while() {
   caffeinate -dims "$@"
 }
 
-quietly_brew_bundle(){
+quietly_brew_bundle() {
   local brewfile=$1
   shift
   local regex='(^Using )|Homebrew Bundle complete|Skipping install of|It is not currently installed|Verifying SHA-256|==> (Downloading|Purging)|Already downloaded:|No SHA-256'
   stay_awake_while brew bundle --no-lock --file="$brewfile" "$@" | (grep -vE "$regex" || true)
 }
 
-is_command(){
+install_packages() {
+  info "Installing or updating Homebrew packages..."
+  quietly_brew_bundle /dev/stdin "$@"
+}
+
+# Search given executable in PATH (remove dependency for `which` command)
+which() {
+  # Alias to Bash built-in command `type -P`
+  type -P "$@"
+}
+
+is_command() {
   command -v "$1" > /dev/null
+}
+
+require_homebrew() {
+  if ! is_command brew; then
+    abort "This script requires Homebrew, which was not found in your PATH"
+  fi
 }
