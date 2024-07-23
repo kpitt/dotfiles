06111ecc9a63027918387b954506eb7687ca0297
Kenny Pitt <kenny.pitt@gmail.com>
2024-07-23 16:14:48 -0400

    vim: Switch to `nvim` as the default editor

--- dot_zsh/configs/editor.zsh
+++ dot_zsh/configs/editor.zsh
@@ -2,5 +2,5 @@
 # Don't set this to `gvim` or `mvim`, it won't work.
 # Don't set this to `gvim -f`, it won't work (unless you wrap it in a script).
 # http://robots.thoughtbot.com/visual-ize-the-future
-export VISUAL=vim
+export VISUAL=nvim
 export EDITOR=$VISUAL
