ad6cc063e87eea64db78d9b2de2c3eae0d0467b8
Kenny Pitt <kenny.pitt@gmail.com>
2024-08-27 21:35:31 -0400

    nvim: Switch to `lazy.nvim` plugin manager

---
+++ dot_config/nvim/lua/config/lazy.lua
@@ -0,0 +1,31 @@
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.uv.fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- The `mapleader` and `maplocalleader` values should be set before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+require("lazy").setup("plugins", {
+  ui = {
+    border = "rounded",
+  },
+  change_detection = {
+    notify = false  -- don't notify about config file changes
+  },
+})
