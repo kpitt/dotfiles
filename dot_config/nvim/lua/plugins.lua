0c1fd200cd4ab672e80f075e8a9f92c4337de77f
Kenny Pitt <kenny.pitt@gmail.com>
2024-09-04 14:20:54 -0400

    nvim: Wider `which-key` popup
    The "helix" preset is a bit cramped for mappings that include a preview,
    such as registers and marks.

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -291,7 +291,7 @@ return {
     "folke/which-key.nvim",
     event = "VeryLazy",
     opts = {
-      preset = "helix",
+      preset = "modern",
       delay = function(ctx)
         return ctx.plugin and 0 or 500
       end,
