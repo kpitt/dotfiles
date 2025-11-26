f85a3c53e40e0d1f2ffa63ab53cbc90a3e25c059
Kenny Pitt <kenny.pitt@gmail.com>
2025-11-26 16:12:42 -0500

    zsh: Remove Jump plugin

--- dot_zsh/configs/jump.zsh
+++
@@ -1,5 +0,0 @@
-# Initialize "gsamokovarov/jump" fuzzy cd utility.
-# This is made availabe for evaluation, but is currently not enabled.
-# if [[ -x "$(command -v jump)" ]]; then
-#     _evalcache jump "$(jump shell zsh)"
-# fi
